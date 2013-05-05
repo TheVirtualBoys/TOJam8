0f008a962d3116baf6cfeeecfa130de737055cb8
Tim McLennan <tim@nothingness.ca>
2013-05-05 01:00:19 -0400

    Switch state+enum added to Game1.cs for state switching

--- Virtual Boys 2013/WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
+++ Virtual Boys 2013/WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
@@ -29,6 +29,14 @@ namespace WindowsGame1
 		GraphicsDeviceManager	graphics;
 		SpriteBatch				spriteBatch;
 
+		private enum State
+		{
+			STATE_SPLASH,
+			STATE_INTRO,
+			STATE_GAMEPLAY,
+			STATE_SCORES
+		}
+		private State state = State.STATE_SPLASH;
 		private const int MAPS_FOREGROUND = 0;
 		private const int MAPS_BACKGROUND = 1;
 
@@ -99,9 +107,9 @@ namespace WindowsGame1
 
 			pixelShiftSize = 2;
 
-            PhysicsSprite sprite = new PhysicsSprite(gameData.animations[1]);
-            sprite.Ani.start();
-            gameData.sprites.Add(sprite);
+			PhysicsSprite sprite = new PhysicsSprite(gameData.animations[1]);
+			sprite.Ani.start();
+			gameData.sprites.Add(sprite);
 		}
 
 		/// <summary>
@@ -139,53 +147,28 @@ namespace WindowsGame1
 				this.Exit();
 
 			// TODO: Add your update logic here
-
-			//walk through and update all layers
-			foreach (Layer layer in gameData.layers)
-			{
-				layer.Update(gameTime);
-			}
-
-			//walk through all the sprites and update them
-			foreach (Sprite sprite in gameData.sprites)
-			{
-				sprite.update(gameTime);
-			}
-
-			base.Update(gameTime);
-
-			UpdateInput();
-
-			//moves the map by 'pixelShiftSize' pixels. to speed up or slow down, call incPixelShiftSize
-			moveRightByPixels(pixelShiftSize);
-		}
-
-		private void UpdateInput()
-		{
 			KeyboardState newKeyState = Keyboard.GetState();
-
-/*			if (GamePad.GetState(PlayerIndex.One).DPad.Left == ButtonState.Pressed)
-				incPixelShiftSize(-1);
-			else if (GamePad.GetState(PlayerIndex.One).DPad.Right == ButtonState.Pressed)
-				incPixelShiftSize(1);
-			else if (newKeyState.IsKeyDown(Keys.Left) && !oldKeyState.IsKeyDown(Keys.Left))
+			switch (state)
 			{
-				//left key was pressed
-				incPixelShiftSize(-1);
+				case State.STATE_GAMEPLAY:
+					gameplayInput(oldKeyState, newKeyState);
+					gameplayUpdate(gameTime);
+				break;
+				case State.STATE_INTRO:
+					introInput(oldKeyState, newKeyState);
+					introUpdate(gameTime);
+				break;
+				case State.STATE_SCORES:
+					scoresInput(oldKeyState, newKeyState);
+					scoresUpdate(gameTime);
+				break;
+				case State.STATE_SPLASH:
+					splashInput(oldKeyState, newKeyState);
+					splashUpdate(gameTime);
+				break;
 			}
-			else if (newKeyState.IsKeyDown(Keys.Right) && !oldKeyState.IsKeyDown(Keys.Right))
-			{
-				//right key was pressed
-				incPixelShiftSize(1);
-			}
-			else if (newKeyState.IsKeyDown(Keys.Up) && !oldKeyState.IsKeyDown(Keys.Up))
-			{
-				audioSys.playSFX(AudioSys.Effect.SFX_LAND);
-			}
-*/
-
-            gameData.sprites[0].input( newKeyState );
-
+			
+			base.Update(gameTime);
 			oldKeyState = newKeyState;
 		}
 
@@ -221,7 +204,7 @@ namespace WindowsGame1
 			int tileSetIndex = gameData.getTileSetIndex(gameData.maps[MAPS_FOREGROUND].tileset);
 			if (tileSetIndex >= 0)
 			{
-                int numRows = numScreenTilesHigh;
+				int numRows = numScreenTilesHigh;
 				int numCols = numScreenTilesWide + 1;	//get the # cols with one extra past screen width so that per pixel shifting doesn't look bad
 				for (int row = 0; row < numRows; ++row)
 				{
@@ -231,25 +214,25 @@ namespace WindowsGame1
 						TileSet tileSet = gameData.getTileSet(mapData.tileset);
 
 						int tileSetRectIndex = getMapDataRectIndex(mapData, row, col);
-                        if (tileSetRectIndex == -1)		//shouldn't draw these tiles so continue
+						if (tileSetRectIndex == -1)		//shouldn't draw these tiles so continue
 							continue;
 
-                        Color color = Color.White;
-                        if (col == numCols / 2)
-                        {
-                            int trueIndex = getMapDataTrueIndex(mapData, row, col);
-                            int boundFlags = (int)tileSet.bounds[trueIndex];
-                            if (boundFlags == 15) color = Color.Red;
-                            if (boundFlags == (int)TileSet.Bounds.BOUNDS_BOTTOM) color = Color.Green;
-/*                            if ( (boundFlags & (int)TileSet.Bounds.BOUNDS_TOP) == 1 ) color = Color.Red;
-                            if ( (boundFlags & (int)TileSet.Bounds.BOUNDS_RIGHT) == 1 ) color = Color.Red;
-                                case TileSet.Bounds.BOUNDS_LEFT: color = Color.Green; break;
-                                case TileSet.Bounds.BOUNDS_BOTTOM: color = Color.Blue; break;
-                                case TileSet.Bounds.BOUNDS_RIGHT: color = Color.Yellow; break;
-                                case TileSet.Bounds.BOUNDS_SLASH: color = Color.Purple; break;
-                                case TileSet.Bounds.BOUNDS_BSLASH: color = Color.Orange; break;
-                            };
-  */                      }
+						Color color = Color.White;
+						if (col == numCols / 2)
+						{
+							int trueIndex = getMapDataTrueIndex(mapData, row, col);
+							int boundFlags = (int)tileSet.bounds[trueIndex];
+							if (boundFlags == 15) color = Color.Red;
+							if (boundFlags == (int)TileSet.Bounds.BOUNDS_BOTTOM) color = Color.Green;
+/*							if ( (boundFlags & (int)TileSet.Bounds.BOUNDS_TOP) == 1 ) color = Color.Red;
+							if ( (boundFlags & (int)TileSet.Bounds.BOUNDS_RIGHT) == 1 ) color = Color.Red;
+								case TileSet.Bounds.BOUNDS_LEFT: color = Color.Green; break;
+								case TileSet.Bounds.BOUNDS_BOTTOM: color = Color.Blue; break;
+								case TileSet.Bounds.BOUNDS_RIGHT: color = Color.Yellow; break;
+								case TileSet.Bounds.BOUNDS_SLASH: color = Color.Purple; break;
+								case TileSet.Bounds.BOUNDS_BSLASH: color = Color.Orange; break;
+							};
+	*/					}
 						Rectangle dims = tileSet.coords[tileSetRectIndex];
 						//NOTE: row * dims.Height only works if ALL tiles have the same height
 						spriteBatch.Draw(tileSet.texture, new Rectangle(col * dims.Width - startingPixelOffset, row * dims.Height, dims.Width, dims.Height), dims, color);
@@ -293,6 +276,60 @@ namespace WindowsGame1
 
 				moveRightByTiles(numTiles);
 			}
+		}
+
+		public void gameplayUpdate(GameTime gameTime)
+		{
+			//walk through and update all layers
+			foreach (Layer layer in gameData.layers)
+			{
+				layer.Update(gameTime);
+			}
+
+			//walk through all the sprites and update them
+			foreach (Sprite sprite in gameData.sprites)
+			{
+				sprite.update(gameTime);
+			}
+
+			//moves the map by 'pixelShiftSize' pixels. to speed up or slow down, call incPixelShiftSize
+			moveRightByPixels(pixelShiftSize);
+		}
+
+		public void introUpdate(GameTime gameTime)
+		{
+
+		}
+
+		public void scoresUpdate(GameTime gameTime)
+		{
+
+		}
+
+		public void splashUpdate(GameTime gameTime)
+		{
+
+		}
+
+		public void gameplayInput(KeyboardState oldKeyState, KeyboardState newKeyState)
+		{
+			gameData.sprites[0].input(newKeyState);
+		}
+
+		public void introInput(KeyboardState oldKeyState, KeyboardState newKeyState)
+		{
+			if (newKeyState.GetPressedKeys().Length > 0) state = State.STATE_GAMEPLAY;
+		}
+
+		public void scoresInput(KeyboardState oldKeyState, KeyboardState newKeyState)
+		{
+			// wait for any input, then return to intro state
+			if (newKeyState.GetPressedKeys().Length > 0) state = State.STATE_INTRO;
+		}
+
+		public void splashInput(KeyboardState oldKeyState, KeyboardState newKeyState)
+		{
+			if (newKeyState.GetPressedKeys().Length > 0) state = State.STATE_INTRO;
 		}
 
 		public TileSet.Bounds ray(int x0, int y0, int x1, int y1, out int boundsX, out int boundsY, Map mapData, TileSet tileSet)
@@ -406,17 +443,17 @@ namespace WindowsGame1
 			return index;
 		}
 
-        public int getMapDataTrueIndex(Map mapData, int screenRow, int screenCol)
-        {
-            int col = screenCol + startingTileOffset;
-            if (col >= gameData.maps[firstMap].width)
-                col -= gameData.maps[firstMap].width;
+		public int getMapDataTrueIndex(Map mapData, int screenRow, int screenCol)
+		{
+			int col = screenCol + startingTileOffset;
+			if (col >= gameData.maps[firstMap].width)
+				col -= gameData.maps[firstMap].width;
 
-            int index = mapData.data[screenRow][col];
-            if (index < -1)
-                index = 0;
-            return index;
-        }
+			int index = mapData.data[screenRow][col];
+			if (index < -1)
+				index = 0;
+			return index;
+		}
 
 		public void moveRightByTiles(int numTiles)
 		{
