8d828677066ffde01bd1106571d9984d8d533d9a
Tim McLennan <tim@nothingness.ca>
2013-05-04 23:00:24 -0400

    update folder structure

---
+++ Virtual Boys 2013/WindowsGame1/WindowsGame1/WindowsGame1/States/BaseState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1
+{
+	public abstract class BaseState
+	{
+		public abstract void start();
+		public abstract void pause();
+		public abstract void resume();
+		public abstract void stop();
+	}
+}
