d671fb281b01758eb5da2f6e24c07ecd2f10360b
chris <chrisr@example.com>
2013-05-05 01:04:22 -0400

    - moving most of the game map rendering data into the new MapLayer class

--- Virtual Boys 2013/WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
+++ Virtual Boys 2013/WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
@@ -29,36 +29,10 @@ namespace WindowsGame1
 		GraphicsDeviceManager	graphics;
 		SpriteBatch				spriteBatch;
 
-		private const int MAPS_FOREGROUND = 0;
-		private const int MAPS_BACKGROUND = 1;
-
-		private int numScreenTilesWide;
-		private int numScreenTilesHigh;
-
-		private const int tileWidth = 16;
-		private const int tileHeight = 16;
-
-		private int pixelShiftSize;
-
-		/**
-		 * The tile column offset into the current tileset
-		 */
-		int startingTileOffset;
-
-		int startingPixelOffset;
-
-		/**
-		 * The first map that is shown on the screen
-		 */
-		int firstMap;
-
-		/**
-		 * The second map that is shown beside the first map
-		 */
-		int secondMap;
-
 		KeyboardState oldKeyState;
 
+		//FIXME: this is temp
+		MapLayer mapLayer;
 
 		public Game1()
 		{
@@ -69,9 +43,6 @@ namespace WindowsGame1
 			Content.RootDirectory = "Content";
 			audioSys = new AudioSys();
 			gameData = new GameData();
-
-			numScreenTilesWide = graphics.PreferredBackBufferWidth / tileWidth;
-			numScreenTilesHigh = graphics.PreferredBackBufferHeight / tileHeight;
 		}
 
 		/// <summary>
@@ -89,19 +60,18 @@ namespace WindowsGame1
 			gameData.init(Content);
 			base.Initialize();
 
+			gameData.ScreenWidth = graphics.PreferredBackBufferWidth;
+			gameData.ScreenHeight = graphics.PreferredBackBufferHeight;
+
 			oldKeyState = Keyboard.GetState();
-
-			startingTileOffset = 0;
-			startingPixelOffset = 0;
-
-			firstMap = MAPS_FOREGROUND;
-			secondMap = MAPS_FOREGROUND;
-
-			pixelShiftSize = 2;
 
             PhysicsSprite sprite = new PhysicsSprite(gameData.animations[1]);
             sprite.Ani.start();
             gameData.sprites.Add(sprite);
+
+			mapLayer = new MapLayer(gameData, 16, 16);
+			gameData.layers.Add(mapLayer);
+			mapLayer.setSpeed(2.25);
 		}
 
 		/// <summary>
@@ -155,9 +125,6 @@ namespace WindowsGame1
 			base.Update(gameTime);
 
 			UpdateInput();
-
-			//moves the map by 'pixelShiftSize' pixels. to speed up or slow down, call incPixelShiftSize
-			moveRightByPixels(pixelShiftSize);
 		}
 
 		private void UpdateInput()
@@ -189,17 +156,6 @@ namespace WindowsGame1
 			oldKeyState = newKeyState;
 		}
 
-		/**
-		 * Changes the pixel shift size by 'inc' (either + or -).
-		 * This makes the map scroll by faster or slower
-		 */
-		private void incPixelShiftSize(int inc)
-		{
-			pixelShiftSize += inc;
-			if (pixelShiftSize < 0)
-				pixelShiftSize = 0;
-		}
-
 		/// <summary>
 		/// This is called when the game should draw itself.
 		/// </summary>
@@ -218,44 +174,6 @@ namespace WindowsGame1
 			}
 
 
-			int tileSetIndex = gameData.getTileSetIndex(gameData.maps[MAPS_FOREGROUND].tileset);
-			if (tileSetIndex >= 0)
-			{
-                int numRows = numScreenTilesHigh;
-				int numCols = numScreenTilesWide + 1;	//get the # cols with one extra past screen width so that per pixel shifting doesn't look bad
-				for (int row = 0; row < numRows; ++row)
-				{
-					for (int col = 0; col < numCols; ++col)
-					{
-						Map mapData = getMapData(row, col);
-						TileSet tileSet = gameData.getTileSet(mapData.tileset);
-
-						int tileSetRectIndex = getMapDataRectIndex(mapData, row, col);
-                        if (tileSetRectIndex == -1)		//shouldn't draw these tiles so continue
-							continue;
-
-                        Color color = Color.White;
-                        if (col == numCols / 2)
-                        {
-                            int trueIndex = getMapDataTrueIndex(mapData, row, col);
-                            int boundFlags = (int)tileSet.bounds[trueIndex];
-                            if (boundFlags == 15) color = Color.Red;
-                            if (boundFlags == (int)TileSet.Bounds.BOUNDS_BOTTOM) color = Color.Green;
-/*                            if ( (boundFlags & (int)TileSet.Bounds.BOUNDS_TOP) == 1 ) color = Color.Red;
-                            if ( (boundFlags & (int)TileSet.Bounds.BOUNDS_RIGHT) == 1 ) color = Color.Red;
-                                case TileSet.Bounds.BOUNDS_LEFT: color = Color.Green; break;
-                                case TileSet.Bounds.BOUNDS_BOTTOM: color = Color.Blue; break;
-                                case TileSet.Bounds.BOUNDS_RIGHT: color = Color.Yellow; break;
-                                case TileSet.Bounds.BOUNDS_SLASH: color = Color.Purple; break;
-                                case TileSet.Bounds.BOUNDS_BSLASH: color = Color.Orange; break;
-                            };
-  */                      }
-						Rectangle dims = tileSet.coords[tileSetRectIndex];
-						//NOTE: row * dims.Height only works if ALL tiles have the same height
-						spriteBatch.Draw(tileSet.texture, new Rectangle(col * dims.Width - startingPixelOffset, row * dims.Height, dims.Width, dims.Height), dims, color);
-					}
-				}
-			}
 
 			//walk through all the sprites and draw them
 			foreach (Sprite sprite in gameData.sprites)
@@ -280,19 +198,6 @@ namespace WindowsGame1
 
 			spriteBatch.End();
 			base.Draw(gameTime);
-		}
-
-		public void moveRightByPixels(int numPixels)
-		{
-			startingPixelOffset += numPixels;
-
-			if (startingPixelOffset >= tileWidth)
-			{
-				int numTiles = startingPixelOffset / tileWidth;
-				startingPixelOffset = startingPixelOffset % tileWidth;
-
-				moveRightByTiles(numTiles);
-			}
 		}
 
 		public TileSet.Bounds ray(int x0, int y0, int x1, int y1, out int boundsX, out int boundsY, Map mapData, TileSet tileSet)
@@ -334,7 +239,8 @@ namespace WindowsGame1
 
 			while (true)
 			{
-				bounds = getMapTileBounds(x0, y0, mapData, tileSet);
+				//FIXME: mapLayer here is local and probably shouldn't be
+				bounds = mapLayer.getMapTileBounds(x0, y0, mapData, tileSet);
 				if (bounds != TileSet.Bounds.BOUNDS_NONE)	//found a collision bounds so return
 				{
 					boundsX = x0;
@@ -360,100 +266,7 @@ namespace WindowsGame1
 			return bounds;
 		}
 
-		private TileSet.Bounds getMapTileBounds(int px, int py, Map mapData, TileSet tileSet)
-		{
-			TileSet.Bounds bounds = TileSet.Bounds.BOUNDS_NONE;
-
-			//get the tile position for the pixel coords
-			int row, col;
-			convertScreenPxToTile(px, py, out row, out col);
-
-			//get the bounds for the tile
-			int tileTypeIndex = mapData.data[row][col];
-			bounds = tileSet.bounds[tileTypeIndex];
-
-			return bounds;
-		}
-
-		public Map getMapData(int screenRow, int screenCol)
-		{
-			int index = getMapDataIndex(screenRow, screenCol);
-			return (index >= 0) ? gameData.maps[index] : null;
-		}
-
-		public int getMapDataIndex(int screenRow, int screenCol)
-		{
-			int col = screenCol + startingTileOffset;
-
-			int mapIndex;
-			if (col < gameData.maps[firstMap].width)
-				mapIndex = firstMap;
-			else
-				mapIndex = secondMap;
-
-			return mapIndex;
-		}
-
-		public int getMapDataRectIndex(Map mapData, int screenRow, int screenCol)
-		{
-			int col = screenCol + startingTileOffset;
-			if (col >= gameData.maps[firstMap].width)
-				col -= gameData.maps[firstMap].width;
-
-			int index = mapData.data[screenRow][col] - 1;
-			if (index < -1)
-				index = 0;
-			return index;
-		}
-
-        public int getMapDataTrueIndex(Map mapData, int screenRow, int screenCol)
-        {
-            int col = screenCol + startingTileOffset;
-            if (col >= gameData.maps[firstMap].width)
-                col -= gameData.maps[firstMap].width;
-
-            int index = mapData.data[screenRow][col];
-            if (index < -1)
-                index = 0;
-            return index;
-        }
-
-		public void moveRightByTiles(int numTiles)
-		{
-			startingTileOffset += numTiles;
-
-			if (startingTileOffset >= gameData.maps[firstMap].width)
-			{
-				//completely moved past the first map, so shift things
-				startingTileOffset -= gameData.maps[firstMap].width;
-				firstMap = secondMap;
-				secondMap = getNextMap();
-			}
-		}
-
-		/**
-		 * Gives the screen pixel of the top-left corner of the tile
-		 */
-		private void convertTileToScreenPx(int row, int col, out int pX, out int pY)
-		{
-			pX = (col - startingTileOffset) * tileWidth - startingPixelOffset;
-			pY = row * tileHeight;
-		}
-
-		private void convertScreenPxToTile(int pX, int pY, out int row, out int col)
-		{
-			col = (pX + startingPixelOffset) / tileWidth + startingTileOffset;
-			row = pY / tileHeight;
-		}
 
 
-		/**
-		 * Returns the map index for the next map to be used
-		 */
-		private int getNextMap()
-		{
-			//TODO: should probably pick a random mapset next
-			return MAPS_FOREGROUND;
-		}
 	}
 }
